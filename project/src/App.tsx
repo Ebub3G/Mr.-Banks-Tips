e4c61b4ae8d32eb4895249b25c81845ee11444af
Godswill Anuo <113176884+Ebub3G@users.noreply.github.com>
2025-06-03 18:31:56 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,3 +1,4 @@
+// project/src/App.tsx
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/layout/Header';
@@ -8,6 +9,11 @@ import BlogPage from './pages/BlogPage';
 import ResultsPage from './pages/ResultsPage';
 import ContactPage from './pages/ContactPage';
 import NotFoundPage from './pages/NotFoundPage';
+import AboutPage from './pages/AboutPage'; // New Import
+import TermsPage from './pages/TermsPage'; // New Import
+import PrivacyPage from './pages/PrivacyPage'; // New Import
+import AllSportsPage from './pages/AllSportsPage'; // New Import
+import PremiumPage from './pages/PremiumPage'; // Existing but ensuring it's here for links
 
 function App() {
   return (
@@ -17,10 +23,15 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/sports" element={<AllSportsPage />} /> {/* New Route */}
             <Route path="/sports/:sportName" element={<SportPage />} />
             <Route path="/blog" element={<BlogPage />} />
             <Route path="/results" element={<ResultsPage />} />
             <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} /> {/* New Route */}
+            <Route path="/terms" element={<TermsPage />} /> {/* New Route */}
+            <Route path="/privacy" element={<PrivacyPage />} /> {/* New Route */}
+            <Route path="/premium" element={<PremiumPage />} /> {/* Existing but good to list explicitly */}
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
