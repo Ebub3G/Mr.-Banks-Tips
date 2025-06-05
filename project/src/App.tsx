0ed7c22f3de86ac9e5ef7b8e75cc8534deb1b447
Godswill Anuo <113176884+Ebub3G@users.noreply.github.com>
2025-06-05 04:21:10 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -13,7 +13,7 @@ import AboutPage from './pages/AboutPage';
 import TermsPage from './pages/TermsPage';
 import PrivacyPage from './pages/PrivacyPage';
 import AllSportsPage from './pages/AllSportsPage'; // Keep AllSportsPage if you want a dedicated list of sports
-import PremiumPage from './pages/PremiumPage';
+// import PremiumPage from './pages/PremiumPage'; // Removed
 
 function App() {
   return (
@@ -33,7 +33,8 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/terms" element={<TermsPage />} />
             <Route path="/privacy" element={<PrivacyPage />} />
-            <Route path="/premium" element={<PremiumPage />} />
+            {/* Removed PremiumPage route */}
+            {/* <Route path="/premium" element={<PremiumPage />} /> */}
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
